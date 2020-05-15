ac3e3a6d42d65ca90a0a46f4f4f8085054de3746
Rist019 <rist019@adminnoMacBook-Air.local>
2020-05-15 18:40:18 +0900

    add v3

--- gym_flatworld/__init__.py
+++ gym_flatworld/__init__.py
@@ -17,3 +17,8 @@ register(
     id='Flatworld-v2',
     entry_point='gym_flatworld.envs:FlatworldEnv',
     max_episode_steps=50)
+
+register(
+    id='Flatworld-v3',
+    entry_point='gym_flatworld.envs:FlatworldEnv',
+    max_episode_steps=70)
